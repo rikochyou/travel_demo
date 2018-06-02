3b5a5c8e3df0491bc6ae8106a0b47909aa0722db
riko <rikochyou@outlook.com>
2018-06-02 11:30:10 +0800

    add home recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" src="https://dimg03.c-ctrip.com/images/100v050000000m7urF774_C_200_200_Q60.jpg">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'https://dimg03.c-ctrip.com/images/100v050000000m7urF774_C_200_200_Q60.jpg',
+        title: '上海迪士尼度假区',
+        desc: '主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'https://dimg03.c-ctrip.com/images/100v050000000m7urF774_C_200_200_Q60.jpg',
+        title: '上海迪士尼度假区',
+        desc: '主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'https://dimg03.c-ctrip.com/images/100v050000000m7urF774_C_200_200_Q60.jpg',
+        title: '上海迪士尼度假区',
+        desc: '主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding : .1rem
+    .item-info
+      flex: 1
+      padding : .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #cccccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #ffffff
+</style>
