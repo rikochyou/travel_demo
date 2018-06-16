93042a5e6d269995123b260dd058c830ad8cb8c6
riko <rikochyou@outlook.com>
2018-06-16 20:14:16 +0800

    finish city-keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-      <home-header></home-header>
-      <home-swiper :list="swiperList"></home-swiper>
-      <home-icons :list="iconList"></home-icons>
-      <home-recommend :list="recommendList"></home-recommend>
-      <home-weekend :list="weekendList"></home-weekend>
+    <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -15,10 +15,11 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
-export default{
+import { mapState } from 'vuex'
+export default {
   name: 'Home',
   components: {
-    HomeHeader: HomeHeader,
+    HomeHeader,
     HomeSwiper,
     HomeIcons,
     HomeRecommend,
@@ -26,11 +27,15 @@ export default{
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo () {
@@ -49,11 +54,18 @@ export default{
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
-
 </script>
 
 <style>
+
 </style>
