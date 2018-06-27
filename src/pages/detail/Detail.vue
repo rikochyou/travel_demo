a1d863121d61e6496318bb57b1c42effa9158616
riko <rikochyou@outlook.com>
2018-06-27 12:30:37 +0800

    detail-header finish

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="page">
     <detail-banner></detail-banner>
-
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
+import DetailHeader from './components/Header.vue'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style scoped lang="stylus">
+  .content
+    height: 50rem
 </style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <div class="header-fixed">
+      景点详情
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~styles/variables.styl'
+  .header-abs
+    position: absolute
+    left: .2rem
+    top: .2rem
+    width: .8rem
+    height: .8rem
+    line-height: .8rem
+    border-radius: .4rem
+    text-align: center
+    background: rgba(0, 0, 0, .8)
+    .header-abs-back
+      color: #ffffff
+      font-size: .4rem
+  .header-fixed
+    z-index: 2
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #ffffff
+    background: $bgColor
+    font-size: .32rem
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
